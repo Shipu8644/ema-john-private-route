1432e2765823164305a509c583be7a0340cf11a0
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-09 23:25:34 +0600

    Firebase added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYJyDQnjyXzCmDl80Yuk5FIoFGy_7vRS4",
+    authDomain: "ema-john-simple-784b5.firebaseapp.com",
+    projectId: "ema-john-simple-784b5",
+    storageBucket: "ema-john-simple-784b5.appspot.com",
+    messagingSenderId: "749911981135",
+    appId: "1:749911981135:web:053ba565959a087546be3a"
+};
+
+export default firebaseConfig;
